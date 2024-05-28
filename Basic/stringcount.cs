cdf8865e0cbd09e5eaf188e70cfbcb78c0acb728
Mahesh Ponnapalli <ponnapallimahesh7@gmail.com>
2024-05-28 18:08:09 +0530

    Add files via upload
    added new file

---
+++ Basic/stringcount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    internal class stringcount
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "hello123";
+            int digitcount = str1.Count(char.IsDigit);
+            int charcount=str1.Count(char.IsLetter);
+            Console.WriteLine(digitcount);
+            Console.WriteLine(charcount);
+
+        }
+    }
+}

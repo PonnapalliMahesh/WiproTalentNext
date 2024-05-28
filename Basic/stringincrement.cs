59b0efda1bf48bb63dff05627ceea0df0dcdef5c
Mahesh Ponnapalli <ponnapallimahesh7@gmail.com>
2024-05-28 18:36:29 +0530

    Add files via upload
    string increment by 1

---
+++ Basic/stringincrement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    internal class stringincrement
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "mahesh";
+            string incstring=new string(str1.Select(c => (char)(c+1)).ToArray());
+            Console.WriteLine(incstring);
+
+        }
+    }
+}

f3145aebe28078d60474211b9198aa0ac7d7c032
21BQ1A6149 - PONNAPALLI MAHESH <21BQ1A6149@vvit.net>
2024-05-28 17:14:10 +0530

    Add project files.

---
+++ Basic/areaRectangle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Basic
+{
+    class areaRectangle
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter side of square:");
+            int a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Area of square:"+a*a);
+        }
+    }
+}

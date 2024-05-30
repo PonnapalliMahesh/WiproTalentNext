33317fd01d4c514dd63adab9aba5d7c846b0a014
Mahesh Ponnapalli <ponnapallimahesh7@gmail.com>
2024-05-30 09:46:19 +0530

    Add files via upload

---
+++ Basic/stringmanipulation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    internal class stringmanipulation
+    {
+        static void Main(string[] args)
+
+        {
+            Console.Write("Enter a String:");
+            string str1=Console.ReadLine();
+            char ch1 = 'e'; 
+            Console.WriteLine(str1);
+            Console.WriteLine(str1.Substring(1,str1.Length-1));
+            Console.WriteLine(str1.Replace(ch1, '$'));
+        }
+    }
+}
